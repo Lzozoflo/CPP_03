48297a1b7d1ab0c5d66c1c0e6a87b661ed14a703
fcretin <fcretin@student.42lyon.fr>
2025-06-01 12:41:02 +0200

    well its cc

--- ex03/src/DiamondTrap.cpp
+++ ex03/src/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: fcretin <fcretin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 12:19:53 by fcretin           #+#    #+#             */
-/*   Updated: 2025/05/31 09:11:44 by fcretin          ###   ########.fr       */
+/*   Updated: 2025/06/01 12:40:59 by fcretin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,6 +83,7 @@ void DiamondTrap::attack(const std::string &target)
 {
 	ScavTrap::attack(target);
 }
+
 
 
 
